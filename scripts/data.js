413eca9ce4d4ca60f0ffb299f1d7f1b885cbf941
NiHorvat <nikolahorvat2004@gmail.com>
2025-03-28 10:53:50 +0100

    dodat grdi gap

---
+++ scripts/data.js
@@ -0,0 +1,30 @@
+const data = {
+    "website": "Hrana",
+    "categories": [
+        { 
+            "name" : "Meso",
+            "image" : "meat.jpg",
+            "products" : [
+                { 
+                    "name" : "Pileći batak 250 grama", "image" : "drumstick250g.jpg"
+                },
+                { 
+                    "name" : "Juneći ramstek 1kg", "image" : "rumpsteak1kg.jpg"
+                },                
+                { 
+                    "name" : "Hrenovke 150 grama", "image" : "sausages150gr.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Slatkiši",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Životinjsko carstvo", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        }
+    ]
+}
+
